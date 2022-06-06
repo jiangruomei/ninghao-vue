8d92c5de685c1e42cbc1e47f98da1fa6a78b3479
jiangruomei <173887979@qq.com>
2022-06-06 21:44:09 +0800

    清理

---
+++ src/app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>{{ name }}</h3>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'MEIMEI',
+    };
+  },
+};
+</script>
+<style>
+@import './styles/app.css';
+</style>

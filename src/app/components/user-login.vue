61dc91fa44b11c11a7df24dbd4aa013452ee9987
jiangruomei <173887979@qq.com>
2022-06-11 17:28:50 +0800

    Vue组件的生命周期方法

--- src/app/components/user-login.vue
+++ src/app/components/user-login.vue
@@ -12,7 +12,34 @@ export default {
       username: '',
     };
   },
-
+  //创建前
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  //创建后
+  created() {
+    console.log('user login created');
+  },
+  //挂载之前
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  //挂载后
+  mounted() {
+    console.log('mounted');
+  },
+  //更新之前
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  //更新后
+  updated() {
+    console.log('updated');
+  },
+  //被卸载后
+  unmounted() {
+    console.log('unmounted');
+  },
   components: {
     AppInput,
   },

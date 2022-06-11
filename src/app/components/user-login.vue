d1904bca41a76e638445614f581cc21b5359a4a6
jiangruomei <173887979@qq.com>
2022-06-11 16:25:03 +0800

    在自定义组件上使用v-model

--- src/app/components/user-login.vue
+++ src/app/components/user-login.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>用户登录</div>
-  <input type="text" />
-  <input type="text" />
+  <div>用户登录 ➡️{{ username }}</div>
+  <AppInput v-model="username" />
+  <!-- <AppInput :model-value="username" @update:model-value="username = $event" /> -->
 </template>
+
+<script>
+import AppInput from './app-input.vue';
+export default {
+  data() {
+    return {
+      username: '',
+    };
+  },
+
+  components: {
+    AppInput,
+  },
+};
+</script>

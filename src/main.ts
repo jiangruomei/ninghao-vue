24b82cbffef89b65047044c1e8594978a68a965b
jiangruomei <173887979@qq.com>
2022-06-29 16:17:00 +0800

    在vue应用里创建与使用Store

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue';
 import App from './app/app.vue';
+import appStore from './app/app.store';
 /**
  * 创建应用
  */
 const app = createApp(App);
+
+/**
+ * 应用 Store
+ */
+app.use(appStore);
 
 /**
  * 挂载应用

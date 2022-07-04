52ff5ff011e67ab98c03ea5143462a947156112a
jiangruomei <173887979@qq.com>
2022-07-04 16:52:55 +0800

    准备一个演示组件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,16 @@
 <template>
-  <h3>{{ name }}</h3>
+  <div class="page">
+    <div class="card">
+      <div class="card_header">
+        <h3 class="card-title">{{ name }}</h3>
+        <div class="card-subtitle">Transition & animation</div>
+      </div>
+      <div class="card_content">
+        <div class="emoji">🌲</div>
+      </div>
+      <div class="card_action"><button>请按这里</button></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +24,5 @@ export default {
 </script>
 <style>
 @import './styles/app.css';
+@import './styles/card.css';
 </style>

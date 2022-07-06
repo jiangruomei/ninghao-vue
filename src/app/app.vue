9e3199676b7b32568e0bb968536bfb8d7a060cd4
jiangruomei <173887979@qq.com>
2022-07-06 16:59:28 +0800

    transition：自定义过渡类

--- src/app/app.vue
+++ src/app/app.vue
@@ -6,9 +6,20 @@
         <div class="card-subtitle">Transition & animation</div>
       </div>
       <div class="card_content">
-        <transition name="pulse">
-          <div v-if="isActive" class="emoji">🎃</div>
+        <transition
+          name="custom-classes"
+          enter-active-class="animate__animated animate__tada"
+          leave-active-class="animate__animated animate__bounce"
+        >
+          <div v-if="isActive" class="emoji">👻</div>
         </transition>
+        <!-- <transition
+          name="custom-classes"
+          enter-active-class="animate__animated animate__tada"
+          leave-active-class="animate__animated animate__bounce"
+        >
+          <div v-if="isActive" class="emoji">👻</div>
+        </transition> -->
       </div>
       <div class="card_action">
         <button @click="isActive = !isActive" :class="{ active: isActive }">
@@ -35,4 +46,5 @@ export default {
 <style>
 @import './styles/app.css';
 @import './styles/card.css';
+@import './styles/animate.css';
 </style>
